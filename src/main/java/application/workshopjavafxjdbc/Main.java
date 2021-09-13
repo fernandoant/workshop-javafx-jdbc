112f227ebbf0d1fcf0170a2e2ed6f7fbfcae42de
Fernando Antonio <fernando.ant22@gmail.com>
2021-09-13 18:55:16 -0300

    Added Main View

---
+++ src/main/java/application/workshopjavafxjdbc/Main.java
@@ -0,0 +1,32 @@
+package application.workshopjavafxjdbc;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("MainView.fxml"));
+
+        ScrollPane scrollPane = fxmlLoader.load();
+
+        scrollPane.setFitToHeight(true);
+        scrollPane.setFitToWidth(true);
+
+        Scene scene = new Scene(scrollPane);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
---
+++ src/main/java/application/workshopjavafxjdbc/MainView.java
@@ -0,0 +1,4 @@
+package application.workshopjavafxjdbc;
+
+public class MainView {
+}
